5ab3c3b41c6a9fcd82b9db5c0cce85917871be76
EJC <ejcsoftwaresolutions@gmail.com>
2023-08-16 14:58:14 -0500

    :wrench:

--- src/main/shared/infrastructure/nestjs/modules/mobile-module.ts
+++ src/main/shared/infrastructure/nestjs/modules/mobile-module.ts
@@ -2,6 +2,7 @@ import { UserJwtStrategy } from '@apps/shared/infrastructure/authentication/stra
 import { Module } from '@nestjs/common';
 import { AppAuthModule } from './app-auth-module';
 import { AuthModule } from './auth-module';
+
 const walkSync = require('walk-sync');
 
 const controllers = discoverControllers();
@@ -20,7 +21,7 @@ type Constructor<T> = any;
 
 export function discoverControllers(): Array<Constructor<any>> {
   return walkSync('apps', {
-    globs: ['mobile/**/*.controller.ts', 'mobile/**/*-controller.ts'],
+    globs: ['alfred/**/*.controller.ts', 'alfred/**/*-controller.ts'],
     includeBasePath: false,
   }).map((p: string) => {
     const filePath = '../../../../../../apps/' + p.slice(0, p.lastIndexOf('.'));
